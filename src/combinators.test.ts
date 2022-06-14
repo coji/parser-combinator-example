776f478f6e692aa0965e00d79963a90ee923b7c5
coji <coji@techtalk.jp>
2022-06-14 15:09:09 +0900

    feat: added "not"

---
+++ src/combinators.test.ts
@@ -0,0 +1,46 @@
+import { describe, test, expect } from 'vitest'
+import { not } from './combinators'
+import { char } from './char'
+import type { ParserOutput } from './types'
+
+describe('not(char("a"))', () => {
+  const parser = not(char('a'))
+
+  test('Empty input', () => {
+    const input = [] as const
+    const output = parser(input)
+    expect(output).toEqual<ParserOutput<null>>({
+      result: 'success',
+      data: null,
+      rest: []
+    })
+  })
+
+  test('Input "a"', () => {
+    const input = [...'a']
+    const output = parser(input)
+    expect(output).toEqual<ParserOutput<null>>({
+      result: 'fail'
+    })
+  })
+
+  test('Input "A"', () => {
+    const input = [...'A']
+    const output = parser(input)
+    expect(output).toEqual<ParserOutput<null>>({
+      result: 'success',
+      data: null,
+      rest: [...'A']
+    })
+  })
+
+  test('input "hoge"', () => {
+    const input = [...'hoge']
+    const output = parser(input)
+    expect(output).toEqual<ParserOutput<null>>({
+      result: 'success',
+      data: null,
+      rest: [...'hoge']
+    })
+  })
+})
